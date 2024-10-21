182505500aee7716288b9748b4ad6de6725dbfed
Michael Mendy <montana@linux.com>
2024-10-21 09:44:24 -0700

    Modernize the NTSB links. Including accident number and CAROL queries.

--- ntsb_crawler.py
+++ ntsb_crawler.py
@@ -1,7 +1,6 @@
-from pyquery import PyQuery as pq
 import requests
-from slugify import slugify 
-
+from pyquery import PyQuery as pq
+from slugify import slugify
 import os
 import time
 import argparse
@@ -11,39 +10,24 @@ import csv
 import datetime
 import re
 
-
 docketurlprefix = "https://data.ntsb.gov/carol-main-public/basic-search"
 docketurlsuffix = "&StartRow=1&EndRow=3000&CurrentPage=1&order=1&sort=0&TXTSEARCHT="
-
 masterurlprefix = "https://dms.ntsb.gov/pubdms/search/"
 detailurlprefix = "https://data.ntsb.gov/"
+sleeptime = 0.5
 
-sleeptime = (
-    0.5 
-)
-
-parser = argparse.ArgumentParser(
-    description="This file attempts to fetch National Transportation Safety Board docket files."
-)
-parser.add_argument(
-    "docketid",
-    metavar="docketid",
-    help="docketID number from NTSB URL for the file you want",
-)
+parser = argparse.ArgumentParser(description="This file attempts to fetch National Transportation Safety Board docket files.")
+parser.add_argument("docketid", metavar="docketid", help="docketID number from NTSB URL for the file you want")
 
 try:
     args = parser.parse_args()
 except:
     parser.print_help()
-    print(
-        "Get the docket ID number from the URL the NTSB gives you with a successful search."
-    )
-    print(
-        "Example: 58493 is the number in http://dms.ntsb.gov/pubdms/search/hitlist.cfm?docketID=58493"
-    )
+    print("Get the docket ID number from the URL the NTSB gives you with a successful search.")
+    print("Example: 58493 is the number in http://dms.ntsb.gov/pubdms/search/hitlist.cfm?docketID=58493")
     sys.exit(1)
-get_input = input
 
+get_input = input
 docketid = args.docketid
 
 if not os.path.exists(docketid):
@@ -53,27 +37,19 @@ docketurl = docketurlprefix + docketid + docketurlsuffix
 print(docketurl)
 raw = requests.get(docketurl)
 html = pq(raw.content)
-
-accidentnumber = (
-    pq(html)("title").text().split()[2]
-) 
+accidentnumber = pq(html)("title").text().split()[2]
 allrows = pq(html)("tr").filter(".odd") + pq(html)("tr").filter(".leave")
 print("Found " + str(len(allrows("tr"))) + " things to download")
 totalpages = 0
 totalphotos = 0
 masterdict = {}
 
-
 for row in allrows("tr"):
     docno = int(pq(pq(row)("td")[0]).text().strip())
     masterdict[docno] = {}
     docdate = pq(pq(row)("td")[1]).text().strip()
-    masterdict[docno]["doctitle"] = pq(
-        pq(row)("td")[2]
-    ).text()  # This needs to get cleaned up
-    masterdict[docno]["docmasterurl"] = masterurlprefix + pq(
-        pq(pq(pq(row)("td")[2])("a"))
-    ).attr("href")
+    masterdict[docno]["doctitle"] = pq(pq(row)("td")[2]).text()
+    masterdict[docno]["docmasterurl"] = masterurlprefix + pq(pq(pq(pq(row)("td")[2])("a"))).attr("href")
     docdate = datetime.datetime.strptime(docdate, "%b %d, %Y")
     docdate = datetime.datetime.strftime(docdate, "%Y-%m-%d")
     masterdict[docno]["docdate"] = docdate
@@ -90,15 +66,7 @@ for row in allrows("tr"):
     masterdict[docno]["docpages"] = docpages
     masterdict[docno]["docphotos"] = docphotos
 
-print(
-    "Trying to download the "
-    + str(len(allrows("tr")))
-    + " files with "
-    + str(totalpages)
-    + " pages and "
-    + str(totalphotos)
-    + " photos"
-)
+print("Trying to download the " + str(len(allrows("tr"))) + " files with " + str(totalpages) + " pages and " + str(totalphotos) + " photos")
 
 for record in masterdict:
     docmasterurl = masterdict[record]["docmasterurl"]
@@ -108,42 +76,29 @@ for record in masterdict:
     try:
         detailurl = detailurlprefix + pq(pq(docmasterhtml)("input")[1]).attr("value")
     except:
-        print(
-            "Multiple download options found. Trying to get high-quality one for this record."
-        )
+        print("Multiple download options found. Trying to get high-quality one for this record.")
         detailurl = detailurlprefix + pq(pq(docmasterhtml)("option")[-2]).attr("value")
     if detailurl == "https://dms.ntsb.gov/View":
-        detailurl = detailurlprefix + re.sub(
-            "(.*')(.*)('.*)", r"\2", pq(pq(docmasterhtml)("input")[1]).attr("onclick")
-        )
-    if (
-        len(detailurl) <= 2 or detailurl == "http://dms.ntsb.gov/Download"
-
+        detailurl = detailurlprefix + re.sub("(.*')(.*)('.*)", r"\2", pq(pq(docmasterhtml)("input")[1]).attr("onclick"))
+    if len(detailurl) <= 2 or detailurl == "http://dms.ntsb.gov/Download":
         try:
-            detailurl = detailurlprefix + pq(pq(docmasterhtml)("option")[-2]).attr(
-                "value"
-            )  
+            detailurl = detailurlprefix + pq(pq(docmasterhtml)("option")[-2]).attr("value")
             print("Trying alternate download link, maybe, from " + docmasterurl)
         except:
             print("Still having problems finding download url from " + docmasterurl)
 
-    time.sleep(sleeptime)  # Wait to avoid pounding server
+    time.sleep(sleeptime)
     masterdict[record]["detailurl"] = detailurl
     localfilename = masterdict[record]["doctitle"]
     docdate = masterdict[record]["docdate"]
-    localfilename = slugify(localfilename)  # Clean up text, eliminate spaces
-    localfilename = docdate + "-" + localfilename  # prepend date
-    localfilename = (
-        localfilename + "." + detailurl.split(".")[-1]
-    )  
-    localfilename = docketid + "/" + localfilename  
+    localfilename = slugify(localfilename)
+    localfilename = docdate + "-" + localfilename
+    localfilename = localfilename + "." + detailurl.split(".")[-1]
+    localfilename = docketid + "/" + localfilename
     masterdict[record]["localfilename"] = localfilename
 
 masterdict = OrderedDict(sorted(masterdict.items(), key=lambda t: t[0]))
 
-
-FileNotFoundError: [Errno 2] No such file or directory:
-    
 for record in masterdict:
     localfilename = masterdict[record]["localfilename"]
     detailurl = masterdict[record]["detailurl"]
@@ -164,7 +119,6 @@ for record in masterdict:
             masterdict[record]["download"] = "Good"
             time.sleep(sleeptime)
             success = True
-
         except:
             print(f"!!! Error trying to write to {localfilename}")
             masterdict[record]["download"] = "Bad"
@@ -182,26 +136,16 @@ for record in masterdict:
                 time.sleep(sleeptime)
                 success = True
             except:
-                print(
-                    "*** This " + detailurl + " thing wasn't working for me. ***"
-                )
-                print(
-                    "*** Try downloading the right version yourself from "
-                    + masterdict[docno]["docmasterurl"]
-                    + " ***"
-                )
+                print("*** This " + detailurl + " thing wasn't working for me. ***")
+                print("*** Try downloading the right version yourself from " + masterdict[docno]["docmasterurl"] + " ***")
                 masterdict[record]["download"] = "Bad"
                 attempts += 1
-                time.sleep(
-                    3 * sleeptime
-                ) 
-                
+                time.sleep(3 * sleeptime)
+
 print("Attempting to build a CSV")
 with open(docketid + ".csv", "w", newline="") as csvfile:
     put = csv.writer(csvfile)
-    headerrowdict = OrderedDict(
-        sorted(masterdict[record].items(), key=lambda t: t[0])
-    )  # using last record. It'll be fine.
+    headerrowdict = OrderedDict(sorted(masterdict[record].items(), key=lambda t: t[0]))
     headerrow = ["accidentnumber", "docketid", "recordnumber"]
     for key in headerrowdict.keys():
         headerrow.append(key)
